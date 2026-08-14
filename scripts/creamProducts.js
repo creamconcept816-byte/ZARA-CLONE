516773cc7339101591149d5d93b1de9b60fcee43
creamconcept816-byte <creamconcept816@gmail.com>
2026-08-13 23:02:42 -0300

    Create creamProducts.js

---
+++ scripts/creamProducts.js
@@ -0,0 +1,208 @@
+// Catalogo real de Cream Concept (creamconcept.com.ar), traido desde Tienda Nube
+// y adaptado a la estructura del sitio (clon de Zara).
+const creamProducts = [
+  {
+        prod_name: "Campera Canva Boxy",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/campera-boxy-f5d78153fe90809dba17780029524723-640-0.webp",
+        price: 89000,
+        strikedOffPrice: 130000,
+        prod_discount: "-32%",
+        section: "Camperas",
+        url: "https://www.creamconcept.com.ar/productos/campera-canva-boxy",
+  },
+  {
+        prod_name: "Campera Cazadora Flap",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/whatsapp-image-2025-05-29-at-12-04-09_0254504d-174a7aab16d54bd55817485328139813-480-0.webp",
+        price: 150000,
+        strikedOffPrice: null,
+        prod_discount: "",
+        section: "Camperas",
+        url: "https://www.creamconcept.com.ar/productos/parka-exp",
+  },
+  {
+        prod_name: "Campera Diadora Classic",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/dia-58706ef5c88a775f5c17583818276400-640-0.webp",
+        price: 119000,
+        strikedOffPrice: 145000,
+        prod_discount: "-18%",
+        section: "Camperas",
+        url: "https://www.creamconcept.com.ar/productos/campera-diadora-classic",
+  },
+  {
+        prod_name: "Jean Corderoy Vulk",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/pant-corder-b02c6e4513075c467117740308377922-480-0.webp",
+        price: 45000,
+        strikedOffPrice: 109000,
+        prod_discount: "-59%",
+        section: "Jeans",
+        url: "https://www.creamconcept.com.ar/productos/jean-corderoy-vulk",
+  },
+  {
+        prod_name: "Jean Darken",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/img_2028-1300d8e1f95b3bcc1e17484441485587-640-0.webp",
+        price: 45000,
+        strikedOffPrice: 78000,
+        prod_discount: "-42%",
+        section: "Jeans",
+        url: "https://www.creamconcept.com.ar/productos/jean-darken",
+  },
+  {
+        prod_name: "Jean Diesel",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/jean-recto-diesel-855a39420be8dfca5217780023101592-480-0.webp",
+        price: 45000,
+        strikedOffPrice: 95000,
+        prod_discount: "-53%",
+        section: "Jeans",
+        url: "https://www.creamconcept.com.ar/productos/jean-diesel-zjp1q",
+  },
+  {
+        prod_name: "Remera Star",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/cream-dc9a0c54bfbdd9f28a17562212031319-480-0.webp",
+        price: 25000,
+        strikedOffPrice: 43000,
+        prod_discount: "-42%",
+        section: "Remeras",
+        url: "https://www.creamconcept.com.ar/productos/remera-star-qc1hz",
+  },
+  {
+        prod_name: "Remera Disco",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/disco-t-cb22158748f960c65117601038627370-640-0.webp",
+        price: 25000,
+        strikedOffPrice: 42000,
+        prod_discount: "-40%",
+        section: "Remeras",
+        url: "https://www.creamconcept.com.ar/productos/remera-disco-rpv2q",
+  },
+  {
+        prod_name: "Remera Flame",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/remera-flame-79921dae8547edf93117601048017189-640-0.webp",
+        price: 27000,
+        strikedOffPrice: 49000,
+        prod_discount: "-45%",
+        section: "Remeras",
+        url: "https://www.creamconcept.com.ar/productos/remera-flame-4nsb6",
+  },
+  {
+        prod_name: "Pantalon Cargo",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/pantalon-cargo-beige-2-2a071b035e7a6e6eb917418034514049-480-0.webp",
+        price: 45000,
+        strikedOffPrice: 79000,
+        prod_discount: "-43%",
+        section: "Pantalones",
+        url: "https://www.creamconcept.com.ar/productos/pantalon-cargo-p5h3h",
+  },
+  {
+        prod_name: "Pantalon Carpintero Raya",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/carpenter-raya-1-f6bbfd1e36c7c431e117404056790732-640-0.webp",
+        price: 45000,
+        strikedOffPrice: 79000,
+        prod_discount: "-43%",
+        section: "Pantalones",
+        url: "https://www.creamconcept.com.ar/productos/pantalon-carpintero-raya",
+  },
+  {
+        prod_name: "Pantalon Oxid",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/super-bagg-ss-23c555f638dd2f50de17618453484643-480-0.webp",
+        price: 45000,
+        strikedOffPrice: 89000,
+        prod_discount: "-49%",
+        section: "Pantalones",
+        url: "https://www.creamconcept.com.ar/productos/pantalon-oxid-oqb2m",
+  },
+  {
+        prod_name: "Borcego Cat",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/bord-c-3d16733db812322cf417485338226394-1024-1024.webp",
+        price: 149000,
+        strikedOffPrice: null,
+        prod_discount: "",
+        section: "Calzado",
+        url: "https://www.creamconcept.com.ar/productos/borcego-cat",
+  },
+  {
+        prod_name: "Buzo Capucha Hype",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/canguro-hype-e3db69a237f0b5a0d917764781606514-480-0.webp",
+        price: 45000,
+        strikedOffPrice: 69000,
+        prod_discount: "-35%",
+        section: "Buzos",
+        url: "https://www.creamconcept.com.ar/productos/buzo-capucha-hype-1q7ax",
+  },
+  {
+        prod_name: "Canguro Cierre Zip",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/dzip-b-5db50b5e260b31b44f17771491753794-480-0.webp",
+        price: 50000,
+        strikedOffPrice: 89000,
+        prod_discount: "-44%",
+        section: "Buzos",
+        url: "https://www.creamconcept.com.ar/productos/canguro-cierre-zip-yt3oe",
+  },
+  {
+        prod_name: "Canguro Capucha Ron",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/6105367b-2d29-4053-a163-0eab9ce9f90b-63a5d7a4bcb65fc2a917765293717888-480-0.webp",
+        price: 50000,
+        strikedOffPrice: 89000,
+        prod_discount: "-44%",
+        section: "Buzos",
+        url: "https://www.creamconcept.com.ar/productos/canguro-capucha-ron-ihrhi",
+  },
+  {
+        prod_name: "Boxer Cream",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/boxer-cream-ce4b5f9e56fbdc2c5c17606378774004-640-0.webp",
+        price: 17900,
+        strikedOffPrice: null,
+        prod_discount: "",
+        section: "Accesorios",
+        url: "https://www.creamconcept.com.ar/productos/boxer-cream-6pilp",
+  },
+  {
+        prod_name: "Gorra Miki",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/whatsapp-image-2025-08-07-at-14-53-22_d720d783-e4bcaf3c182dcaee1b17545896296634-480-0.webp",
+        price: 35000,
+        strikedOffPrice: null,
+        prod_discount: "",
+        section: "Gorras",
+        url: "https://www.creamconcept.com.ar/productos/gorra-miki",
+  },
+  {
+        prod_name: "Bermuda Baggy Costal",
+        imgUrl:
+                "https://acdn-us.mitiendanube.com/stores/005/200/262/products/bermuda-camou-t-3664aa00e53092a56e17707413064428-480-0.webp",
+        price: 59500,
+        strikedOffPrice: 85000,
+        prod_discount: "-30%",
+        section: "Bermudas",
+        url: "https://www.creamconcept.com.ar/productos/bermuda-baggy-costal-z4c8q",
+  },
+  ];
+
+const creamSections = [
+    "Todos",
+    "New In",
+    "Camperas",
+    "Buzos",
+    "Jeans",
+    "Remeras",
+    "Pantalones",
+    "Bermudas",
+    "Calzado",
+    "Gorras",
+    "Accesorios",
+  ];
